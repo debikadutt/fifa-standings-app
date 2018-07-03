91d0541ba21227edfa93fd2d5254a3536c16d44a
Debika Dutt <debikadutt@gmail.com>
2018-07-03 11:50:56 -0700

    updated website

--- src/containers/GroupTable.js
+++ src/containers/GroupTable.js
@@ -1,24 +1,26 @@
 import React, { Component } from 'react';
 import '../styles/bootstrap.css';
+import '../styles/app.css'
+import logo from '../images/FIFA_World_Cup.png';
 
 class GroupTable extends Component {
   render() {
     return (
         <div className="card card-cascade narrower mt-5">
             <div className="gradient-card-header narrower py-4 d-flex justify-content-center align-items-center">
-
-<h4 className="white-text font-weight-bold text-uppercase">World Cup Fifa 2018</h4>
-
-</div>
-            <div className="d-flex font-weight-bold justify-content-center align-items-center">{this.props.tabledata.name}</div>
+                <img src={logo} className="app-logo" alt=""/>
+                <h4 className="white-text font-weight-bold text-uppercase">Fifa World Cup 2018</h4>
+            </div>
+            <h5 className="d-flex font-weight-bold justify-content-center align-items-center">{this.props.tabledata.name}</h5>
             <table className="table">
                 <thead>
                     <tr>
                         <th scope="row">#</th>
                         <th className="th-lg">Team Name</th>
-                        <th className="th-lg">Position</th>
                         <th className="th-lg">Played</th>
                         <th className="th-lg">Won</th>
+                        <th className="th-lg">Drawn</th>
+                        <th className="th-lg">Lost</th>
                         <th className="th-lg">Points</th>
                     </tr>
                 </thead>
@@ -27,9 +29,10 @@ class GroupTable extends Component {
                         <tr>
                             <th scope="row">{index+1}</th>
                             <td>{teamData.team.name}</td>
-                            <td>{teamData.pos}</td>
                             <td>{teamData.played}</td>
                             <td>{teamData.won}</td>
+                            <td>{teamData.drawn}</td>
+                            <td>{teamData.lost}</td>
                             <td>{teamData.pts}</td>
                         </tr>
                     </tbody>         
